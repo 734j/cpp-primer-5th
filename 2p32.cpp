b7819b597e1241dd98643eeda4c288a341142311
Oskar <open7software@gmail.com>
2024-07-30 17:19:15 +0200

    2.32

---
+++ 2p32.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "sales_item.hpp"
+
+/*
+ *
+ * 2.32
+ *
+ *
+ */
+
+int main () {
+
+	int null = 0, *p = null;
+	std::cout << null << " " << *p << std::endl;
+	return 0;
+}
