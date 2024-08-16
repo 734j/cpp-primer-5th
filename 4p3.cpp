6ff70adb810f1e194892398656e71f979f088feb
Oskar <open7software@gmail.com>
2024-08-16 16:07:33 +0200

    more

---
+++ 4p3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/*
+ *
+ * 4.3
+ *
+ *
+ */
+
+int main () {
+
+	// Sure, i think its acceptable. But i think this is a little out of my depth to really have a good opinion.
+	// As long as i am warned by the compiler that it's undefined then i think its alright.
+	return 0;
+}
---
+++ evaluation-test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/*
+ *
+ * 
+ *
+ *
+ */
+
+int main () {
+
+	//int i = 0;
+	//std::cout << i << " " << ++i << std::endl;
+	return 0;
+}
