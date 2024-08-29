9094c8aa159ee2386fa1d2ff19cbdc73f321303b
Oskar <open7software@gmail.com>
2024-08-29 13:37:00 +0200

    forgot to do this exercise for some reason

--- 5p3.cpp
+++ 5p3.cpp
@@ -2,11 +2,17 @@
 
 /*
  *
- * Description
+ * 5.3
  *
  *
  */
 
 int main () {
+
+	int sum = 0;
+	int val = 1;
+	while (val <= 10 && (sum += val, ++val));
+
+	std::cout << sum << std::endl;
 	return 0;
 }
