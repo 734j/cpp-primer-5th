345015f86c91da53a595e52a9f3acbab07f8b461
Oskar <open7software@gmail.com>
2024-09-22 17:04:00 +0200

    more

---
+++ 7p20.cpp
@@ -0,0 +1,14 @@
+
+/*
+ *
+ * 7.20
+ *
+ *
+ */
+
+int main () {
+
+	// A friend is able to access private members of a class
+	// Not sure about pros and cons
+	return 0;
+}
---
+++ 7p21.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/*
+ *
+ * 7.21
+ *
+ * Done in sales_data.hpp
+ */
+
+int main () {
+
+	
+	return 0;
+}
