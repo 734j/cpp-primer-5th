c62113e55e2d3d06758b050d80d6104a857f68b8
Oskar <open7software@gmail.com>
2025-05-22 10:18:17 +0200

    9.45

--- 9p45.cpp
+++ 9p45.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
+#include <string>
 
 /*
  *
- * Description
- *
+ * I'll be honest, i don't get this exercise. 
+ * It's pretty weirdly explained. I'll just do something i think is right.
  *
  */
 
-int main () {
+std::string prefix_suffix_creator(const std::string name,
+								  const std::string prefix,
+								  const std::string suffix) {
+
+	std::string _name = name;
+	std::string _prefix = prefix + " ";
+	std::string _suffix = " " + suffix;
+	_name.insert(_name.cbegin(), _prefix.cbegin(), _prefix.cend());
+	_name.append(_suffix);
+	return _name;
+}
+
+int main() {
+
+	std::cout << prefix_suffix_creator("Michael", "Mr.", "VII") << std::endl;	
+	std::string name;
+	std::string prefix;
+	std::string suffix;
+	std::cin >> name >> prefix >> suffix;
+	std::cout << prefix_suffix_creator(name, prefix, suffix) << std::endl;
 	return 0;
 }
