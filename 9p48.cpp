5c13b47da262e29c896467d8909dd38b35fa5f65
Oskar <open7software@gmail.com>
2025-05-22 19:27:49 +0200

    9.48 and 9.49

--- 9p48.cpp
+++ 9p48.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
+#include <string>
 
 /*
- *
- * Description
- *
- *
+ * 9.48
+ * 
+ * It returns npos.
+ * We can also test this below
  */
 
 int main () {
+
+	std::string numbers = "0123456789";
+	std::string name = "r2d2";
+	auto result = numbers.find(name);
+	if(result == std::string::npos) {
+		std::cout << "npos was found" << std::endl;
+	} else {
+		std::cout << "npos not found" << std::endl;
+	}
+	
 	return 0;
 }
--- 9p49.cpp
+++ 9p49.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
+#include <string>
 
 /*
  *
- * Description
+ * 9.49
  *
  *
  */
 
+unsigned long find_word_non_ascender_descender(std::string word) {
+
+	std::string asc_desc = "qyijpdfghjlbtk";
+	auto a = word.find_first_of(asc_desc);
+	return a;
+}
+
 int main () {
+
+	std::string word;
+	std::string longest_word;
+	while(std::cin >> word) {
+		if(find_word_non_ascender_descender(word) != std::string::npos) {
+			continue;
+		}
+
+		if(word.size() > longest_word.size()) {
+			longest_word = word;
+		}
+		
+	}
+
+	std::cout << "longest non ascender/descender word: " << longest_word << std::endl;
 	return 0;
 }
